f066118f9ccebdbe1759d222386f1bb86248e23b
stellaura <stellaura@outlook.com>
2024-07-01 16:00:04 +0800

    finish initial CmakeList.txt

---
+++ cmake-build-release/CMakeFiles/DNS-Relay.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/stellaura/Programs/c++/DNS-Relay/build/bin/DNS-Relay"
+  "/home/stellaura/Programs/c++/DNS-Relay/build/bin/DNS-Relay.pdb"
+  "CMakeFiles/DNS-Relay.dir/src/test.cpp.o"
+  "CMakeFiles/DNS-Relay.dir/src/test.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DNS-Relay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
