57f0d08e41335b1f089e5e51f72e08faceda25d5
stellaura <stellaura@outlook.com>
2024-07-01 16:08:20 +0800

    add test header

--- src/test.cpp
+++ src/test.cpp
@@ -2,6 +2,9 @@
 // Created by Leoevergarden on 2024/7/1.
 //
 #include <iostream>
+#include "add.h"
+
 int main(){
-    printf("Hello world");
+    printf("Hello world\n");
+    std::cout << add(1.0,2.5) << std::endl;
 }
