eeef639c4d331af6a8428e930e504608306d792f
tianmu <yinmutian@bupt.edu.cn>
2024-07-02 16:50:49 +0800

    delete add and sub.

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,6 @@
 //
 // Created by Leoevergarden on 2024/7/1.
 //
-#include "add.h"
-#include "sub.h"
 
 #include <catch2/catch_all.hpp>
 #include <fmt/format.h>
@@ -16,7 +14,4 @@ TEST_CASE("test for catch2", "[test]") {
             << FMT_VERSION / 100 % 100 << "." << FMT_VERSION % 100 << std::endl;
   std::cout << "spdlog version: " << SPDLOG_VER_MAJOR << "." << SPDLOG_VER_MINOR
             << "." << SPDLOG_VER_PATCH << std::endl;
-
-  REQUIRE(3 == add(1, 2));
-  REQUIRE(-1 == sub(1, 2));
 }
