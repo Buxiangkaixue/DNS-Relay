e4e5db9dddcc4c38faebafb4ec9bcdca79b37b48
tianmu <yinmutian@bupt.edu.cn>
2024-07-02 16:43:52 +0800

    fix DNSResolver

---
+++ src/DNSResolver.cpp
@@ -0,0 +1,54 @@
+//
+// Created by 天牧 on 24-7-2.
+//
+
+#include "DNSResolver.h"
+#include <arpa/inet.h>
+#include <cstring>
+#include <iostream>
+#include <netdb.h>
+#include <string>
+#include <sys/socket.h>
+#include <vector>
+
+std::pair<std::vector<std::string>, std::vector<std::string>>
+resolve_hostname(const std::string &hostname) {
+    addrinfo hints, *res, *p;
+    int status;
+    char ipstr[INET6_ADDRSTRLEN];
+
+    std::vector<std::string> ipv4_addresses;
+    std::vector<std::string> ipv6_addresses;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((status = getaddrinfo(hostname.c_str(), NULL, &hints, &res)) != 0) {
+        std::cerr << "getaddrinfo: " << gai_strerror(status) << std::endl;
+        return {ipv4_addresses, ipv6_addresses};
+    }
+
+    for (p = res; p != NULL; p = p->ai_next) {
+        void *addr;
+        std::string ipver;
+
+        if (p->ai_family == AF_INET) { // IPv4
+            sockaddr_in *ipv4 = (sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+            inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+            ipv4_addresses.push_back(ipstr);
+        } else { // IPv6
+            sockaddr_in6 *ipv6 = (sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+            inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+            ipv6_addresses.push_back(ipstr);
+        }
+    }
+
+    freeaddrinfo(res); // free the linked list
+
+    return {ipv4_addresses, ipv6_addresses};
+}
