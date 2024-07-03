bbe7462f887da5d5b7f6dd29cd54fe9518c8ba4d
stellaura <stellaura@outlook.com>
2024-07-03 21:24:40 +0800

    LRU K Cache is thread safe

--- include/LRU_K_Cache.h
+++ include/LRU_K_Cache.h
@@ -4,11 +4,13 @@
 #include "LRU_Cache.h"
 #include <stdexcept>
 
-template <typename Key, typename Value, size_t K> class LRU_K_Cache {
+template <typename Key, typename Value, size_t K>
+class LRU_K_Cache {
 private:
   LRU_Cache<Key, std::pair<Value, size_t>> tempCache_; // 包含访问计数
   LRU_Cache<Key, Value> permanentCache_;               // 持久缓存
   size_t capacity_;
+  mutable std::shared_mutex mutex_;
 
   void promoteToPermanent(const Key &key,
                           std::pair<Value, size_t> &valueCountPair) {
@@ -27,6 +29,7 @@ public:
         permanentCache_(capacity - capacity / 10) {}
 
   Value get(const Key &key) {
+    std::unique_lock lock(mutex_);
     if (permanentCache_.contains(key)) {
       return permanentCache_.get(key);
     } else if (tempCache_.contains(key)) {
@@ -39,6 +42,7 @@ public:
   }
 
   void put(const Key &key, const Value &value) {
+    std::unique_lock lock(mutex_);
     if (permanentCache_.contains(key)) {
       permanentCache_.put(key, value);
     } else if (tempCache_.contains(key)) {
